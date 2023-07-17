c5a899f098b4aa48d11bd8e4419994a5955614fb
Rost <emelman@me.com>
2023-07-18 01:30:39 +0300

    Add books

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import './App.css'
-import { Routes, Route } from "react-router"
-import Home from './routes/home'
-import BookShelf from './routes/bookshelf'
-
+import "./App.css";
+import { Routes, Route } from "react-router";
+import Home from "./routes/home";
+import BookShelf from "./routes/bookshelf";
 
 function App() {
-
   return (
-
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/bookshelf' element={<BookShelf />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/bookshelf" element={<BookShelf />} />
     </Routes>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,19 +1,16 @@
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import "./_home.css";
 
 const TITLE = "Home";
 
 function Home() {
-  
-    return (
-      <>
-        <Helmet title={TITLE} />
-        <h1>Clear as Vodka</h1>
-        <p className="read-the-docs">
-          Distilled from the heart
-        </p>
-      </>
-    )
-  }
-  
-  export default Home
+  return (
+    <>
+      <Helmet title={TITLE} />
+      <h1>Clear as Vodka</h1>
+      <p className="read-the-docs">Distilled from the heart</p>
+    </>
+  );
+}
+
+export default Home;
