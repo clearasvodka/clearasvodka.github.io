4d03d46a0f4795ef5a6ff8ee1ff4740e287a34cb
Rost Oso <emelman@me.com>
2023-12-24 02:37:39 +0300

    Add grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <div className="promo-container">
       <h1>Clearasvodka</h1>
-    </main>
+    </div>
   );
 }
