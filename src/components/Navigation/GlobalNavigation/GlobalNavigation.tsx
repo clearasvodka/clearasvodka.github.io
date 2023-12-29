b0c2bc9654041a5bafeaa6df4882e8afeb3c9dd0
Rost Oso <emelman@me.com>
2023-12-30 00:47:05 +0300

    Mobile layot support

--- src/components/Navigation/GlobalNavigation/GlobalNavigation.tsx
+++ src/components/Navigation/GlobalNavigation/GlobalNavigation.tsx
@@ -3,23 +3,21 @@ import Link from "next/link";
 
 export default function GlobalNavigation() {
   return (
-    <>
-      <header>
-        <nav>
-          <ul className="nav-container">
-            <li className="nav-item">
-              <Link className="nav-link" href="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className="nav-link" href="/bookshelf">
-                Bookshelf
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <header>
+      <nav>
+        <ul className="nav-container">
+          <li className="nav-item">
+            <Link className="nav-link" href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" href="/bookshelf">
+              Bookshelf
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
