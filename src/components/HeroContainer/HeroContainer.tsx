14b71eec4e420ded1cae356061b1f485015bb946
Rost Oso <emelman@me.com>
2024-02-27 22:53:17 +0300

    add graphics

--- src/components/HeroContainer/HeroContainer.tsx
+++ src/components/HeroContainer/HeroContainer.tsx
@@ -1,12 +1,27 @@
 import "./heroContainer.css";
+import Image from "next/image";
+import cavSymbol3D from "../../../public/img/img_hero_cav.png";
 
 export default function HeroContainer() {
   return (
     <>
       <div className="hero-container">
         <div className="hero-content">
-          <h1>clearasvodka</h1>
-          <p>BETA</p>
+          <div className="hero-text">
+            <h1>clearasvodka</h1>
+            <span className="text-beta">BETA</span>
+          </div>
+          <div className="hero-image">
+            <Image
+              placeholder="blur"
+              src={cavSymbol3D}
+              alt="clearasvodka logo"
+              style={{
+                maxWidth: "80%",
+                height: "auto",
+              }}
+            />
+          </div>
         </div>
       </div>
     </>
